444cac7459f5f7956b0901beb8676497ef009655
theekshana56 <Theekshanagamage056@gmail.com>
2025-08-17 14:37:18 +0530

    deleted

--- backend/models/AuditLog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const auditSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  action: { type: String, required: true }, // e.g., 'login', 'profile_update'
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('AuditLog', auditSchema);
