030a8d56aa183b157aba1fcf67836337bc962f18
Theekshana <75977363+theekshana56@users.noreply.github.com>
2025-08-22 09:40:09 +0530

    Delete backend directory

--- backend/models/ServiceAdvisor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const serviceAdvisorSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  // Add more fields if needed
-});
-
-module.exports = mongoose.model('ServiceAdvisor', serviceAdvisorSchema);
