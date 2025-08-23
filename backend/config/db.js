e628ea26ab3145a38656c0eb7283091a7b225a83
theekshana56 <Theekshanagamage056@gmail.com>
2025-08-23 10:58:42 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb://localhost:27017/AutoElite');
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err);
