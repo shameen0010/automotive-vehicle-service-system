33990ae1f41dbb46a047d984124d4bc32df04ee2
Theekshana <75977363+theekshana56@users.noreply.github.com>
2025-08-29 02:30:57 +0530

    Delete server directory

--- server/src/models/AuditLog.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const auditLogSchema = new mongoose.Schema({
-  actor: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  action: String,
-  meta: Object
-}, { timestamps: true });
-export default mongoose.model('AuditLog', auditLogSchema);
